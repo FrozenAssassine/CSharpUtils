776e06eba92cf78975f219465e219cf0023396d0
Julius Kirsch <Julius@skirsch.de>
2023-06-14 17:21:38 +0200

    Updated documentation

--- CSharpHelper/Extensions/SafeString.cs
+++ CSharpHelper/Extensions/SafeString.cs
@@ -73,25 +73,61 @@ namespace CSharpHelper.Extensions
         {
             return SafeRemove(str, start, str.Length - start);
         }
-    
+
+        /// <summary>
+        /// Get the index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The index of the found regex or -1 when nothing was found</returns>
         public static int IndexOfRegex(this string str, string pattern, int start, RegexOptions options)
         {
-            var match = Regex.Match(start < 0 || start >= str.Length ? str : str.Substring(start), pattern);
+            var match = Regex.Match(start < 0 || start >= str.Length ? str : str.Substring(start), pattern, options);
             return match.Success ? match.Index : -1;
         }
+        /// <summary>
+        /// Get the index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <returns>The index of the found regex or -1 when nothing was found</returns>
         public static int IndexOfRegex(this string str, string pattern, int start)
         {
             return IndexOfRegex(str, pattern, start, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <returns>The index of the found regex or -1 when nothing was found</returns>
         public static int IndexOfRegex(this string str, string pattern)
         {
             return IndexOfRegex(str, pattern, -1, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The index of the found regex or -1 when nothing was found</returns>
         public static int IndexOfRegex(this string str, string pattern, RegexOptions options)
         {
             return IndexOfRegex(str, pattern, -1, options);
         }
 
+        /// <summary>
+        /// Get the a list of indices of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int[] AllIndexOfRegex(this string str, string pattern, int start, RegexOptions options)
         {
             int MatchToIndex(Match m)
@@ -99,58 +135,97 @@ namespace CSharpHelper.Extensions
                 return m.Index;
             }
 
-            MatchCollection matches = Regex.Matches(start < 0 || start >= str.Length ? str : str.Substring(start), pattern);
+            MatchCollection matches = Regex.Matches(start < 0 || start >= str.Length ? str : str.Substring(start), pattern, options);
             return matches.Count > 0 ? Array.ConvertAll(matches.Where(x => x.Success).ToArray(), new Converter<Match, int>(MatchToIndex)) : Array.Empty<int>();
         }
+        /// <summary>
+        /// Get the a list of indices of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int[] AllIndexOfRegex(this string str, string pattern, int start)
         {
             return AllIndexOfRegex(str, pattern, start, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the a list of indices of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int[] AllIndexOfRegex(this string str, string pattern)
         {
             return AllIndexOfRegex(str, pattern, -1, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the a list of indices of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int[] AllIndexOfRegex(this string str, string pattern, RegexOptions options)
         {
             return AllIndexOfRegex(str, pattern, -1, options);
         }
 
         /// <summary>
-        /// 
+        /// Get the the last index of the given regex pattern
         /// </summary>
-        /// <param name="str"></param>
-        /// <param name="pattern"></param>
-        /// <param name="start"></param>
-        /// <param name="options"></param>
-        /// <returns></returns>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int LastIndexOfRegex(this string str, string pattern, int start, RegexOptions options)
         {
-            string stR = "";
             MatchCollection matches = Regex.Matches(start < 0 || start >= str.Length ? str : str.Substring(start), pattern, options);
             if(matches.Count > 0)
                 return matches[matches.Count - 1].Index;
             return -1;
         }
+        /// <summary>
+        /// Get the the last index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="start">The index after it starts searching</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int LastIndexOfRegex(this string str, string pattern, int start)
         {
             return LastIndexOfRegex(str, pattern, start, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the the last index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int LastIndexOfRegex(this string str, string pattern)
         {
             return LastIndexOfRegex(str, pattern, -1, RegexOptions.None);
         }
+        /// <summary>
+        /// Get the the last index of the given regex pattern
+        /// </summary>
+        /// <param name="str">The string to search in</param>
+        /// <param name="pattern">The regex pattern to search for</param>
+        /// <param name="options">The options for the regex pattern</param>
+        /// <returns>The indices of the found regex or -1 when nothing was found</returns>
         public static int LastIndexOfRegex(this string str, string pattern, RegexOptions options)
         {
             return LastIndexOfRegex(str, pattern, -1, options);
         }
-    
+
         /// <summary>
-        /// Insert a given string at a given position
+        /// Inserts the given string at the specified position in the input string.
         /// </summary>
-        /// <param name="str">The string to insert to</param>
-        /// <param name="startIndex">The zero based position to insert at</param>
-        /// <param name="value">The string to insert at the position</param>
-        /// <returns>A new string with the given word inserted at the given position</returns>
+        /// <param name="str">The input string to insert into.</param>
+        /// <param name="startIndex">The zero-based position at which to insert the string.</param>
+        /// <param name="value">The string to insert at the specified position.</param>
+        /// <returns>A new string with the specified string inserted at the specified position.</returns>
         public static string SafeInsert(this string str, int startIndex, string value)
         {
             if (str == null)
@@ -162,13 +237,13 @@ namespace CSharpHelper.Extensions
             return str.Insert(startIndex < 0 ? 0 : startIndex, value);
         }
         /// <summary>
-        /// Replace the given range with the given string
+        /// Replaces a specified range of characters in the input string with the given string.
         /// </summary>
-        /// <param name="str">The string to replace at</param>
-        /// <param name="startIndex">The zero based start position to replace with</param>
-        /// <param name="length">The length of the characters to replace</param>
-        /// <param name="value">The string to replace the removed with</param>
-        /// <returns>A new string where the given string is added</returns>
+        /// <param name="str">The input string to perform the replacement on.</param>
+        /// <param name="startIndex">The zero-based start position of the range to replace.</param>
+        /// <param name="length">The length of the range of characters to replace.</param>
+        /// <param name="value">The string to replace the removed range with.</param>
+        /// <returns>A new string where the specified range is replaced with the given string.</returns>
         public static string ReplaceRange(this string str, int startIndex, int length, string value)
         {
             return str.SafeRemove(startIndex, length).SafeInsert(startIndex, value);
