17ef55308295c173292e3c27f2eea976316cef73
Julius Kirsch <Julius@skirsch.de>
2023-06-14 17:22:04 +0200

    Added EnumerableExtensions and StringExtensions

---
+++ CSharpHelper/Extensions/EnumerableExtension.cs
@@ -0,0 +1,71 @@
+﻿namespace CSharpHelper.Extensions
+{
+    public static class EnumerableExtension
+    {
+        /// <summary>
+        /// Counts the total number of words in the collection of strings.
+        /// </summary>
+        /// <param name="items">The collection of strings to count the words from.</param>
+        /// <returns>The total number of words in the collection.</returns>
+
+        public static int CountWords(this IEnumerable<string> items)
+        {
+            int words = 0;
+            var separator = new[] { ' ', '\n', '\r' };
+            foreach (string line in items)
+            {
+                words += line.Split(separator, StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+            return words;
+        }
+
+        /// <summary>
+        /// Counts the total number of characters in the collection of strings.
+        /// </summary>
+        /// <param name="items">The collection of strings to count the characters from.</param>
+        /// <returns>The total number of characters in the collection.</returns>
+        public static int CountCharacters(this IEnumerable<string> items)
+        {
+            int characters = 0;
+            foreach (string line in items)
+            {
+                characters += line.Length;
+            }
+            return characters;
+        }
+
+        /// <summary>
+        /// Returns the index of the string with the longest length in the provided collection.
+        /// </summary>
+        /// <param name="items">The collection of strings to search.</param>
+        /// <returns>The index of the string with the longest length. If the collection is empty, returns 0.</returns>
+        public static int LongestStringIndex(IEnumerable<string> items)
+        {
+            int longestIndex = 0;
+            int oldLenght = 0;
+            int i = 0;
+            foreach(var item in items)
+            {
+                i++;
+                var lenght = item.Length;
+                if (lenght > oldLenght)
+                {
+                    longestIndex = i;
+                    oldLenght = lenght;
+                }
+            }
+            return longestIndex;
+        }
+
+        /// <summary>
+        /// Concatenates the strings in the collection using the specified separator and returns the resulting string.
+        /// </summary>
+        /// <param name="items">The collection of strings to concatenate.</param>
+        /// <param name="separator">The string used to separate each element in the resulting concatenated string.</param>
+        /// <returns>A string that consists of the elements in the collection concatenated using the specified separator.</returns>
+        public static string ToString(this IEnumerable<string> items, string separator)
+        {
+            return string.Join(separator, items);
+        }
+    }
+}
---
+++ CSharpHelper/Extensions/StringExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpHelper.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Counts the total number of words in the provided string.
+        /// </summary>
+        /// <param name="text">The string to count the words from.</param>
+        /// <returns>The total number of words in the string.</returns>
+        public static int CountWords(this string text)
+        {
+            if (text.Contains("\r") || text.Contains("\r\n"))
+                return text.Count(x => x == ' ' || x == '\r') + 1;
+            return text.Count(x => x == ' ' || x == '\n') + 1;
+        }
+
+        /// <summary>
+        /// Repeats the provided string a specified number of times.
+        /// </summary>
+        /// <param name="text">The string to repeat.</param>
+        /// <param name="count">The number of times to repeat the string.</param>
+        /// <returns>A new string consisting of the input string repeated the specified number of times.</returns>
+        public static string Repeat(this string text, int count)
+        {
+            return string.Join("", Enumerable.Repeat(text, count));
+        }
+    }
+}
